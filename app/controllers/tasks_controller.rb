bcd102c09f7e140f37661da211733fda5a89450b
mame.taro <mametaro.sd10@gmail.com>
2020-09-22 13:37:36 +0000

    tasklist

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,11 @@
 class TasksController < ApplicationController
   
+  before_action :require_user_logged_in
   before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:destroy, :update] 
   
   # コントローラファイルに定義されているこれらのアクションを実行する
-  def index 
+  def index
     @tasks = Task.all
   end 
   
@@ -11,11 +13,13 @@ class TasksController < ApplicationController
   end 
   
   def new
-    @task = Task.new
+      # @task = Task.new
+      @task = current_user.tasks.build
   end 
   
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
+    # @task = Task.new(task_params)
     
     if @task.save
       flash[:success] = 'Taskが正常に投稿されました'
@@ -42,7 +46,6 @@ class TasksController < ApplicationController
   
   def destroy
     @task.destroy
-    
     flash[:success] = 'Taskは正常に削除されました'
     redirect_to tasks_url
   end 
@@ -56,5 +59,12 @@ class TasksController < ApplicationController
   # Strong Parameter 
   def task_params
     params.require(:task).permit(:content, :status)
+  end 
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end 
   end 
 end
